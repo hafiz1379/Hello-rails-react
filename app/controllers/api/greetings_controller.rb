a0192c849ffca5e89d2cad059f2d4e17c951b16c
hafiz1379 <hafizrasa1379@gmail.com>
2023-11-27 17:27:36 +0430

    Refactor greetings_controller.rb to display random greeting in HTML format

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,6 +1,10 @@
-class Api::GreetingsController < ApplicationController
-  def random
-    random_greeting = Message.order('RANDOM()').first
-    render json: { message: random_greeting.content }
+# app/controllers/api/greetings_controller.rb
+module Api
+  class GreetingsController < ApplicationController
+    def random
+      random_message = Message.order(Arel.sql('RANDOM()')).first
+      @greeting = random_message&.content
+      render html: "<p style='color: green; font-size: 30px; text-align: center;'>#{@greeting}</p>".html_safe
+    end
   end
 end
