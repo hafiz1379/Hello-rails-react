bb2fb5f2e6eaa1a4359f8532492f340c2e65f52b
hafiz1379 <hafizrasa1379@gmail.com>
2023-11-27 16:39:41 +0430

    Update application.js to use React and Redux

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import App from './components/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+);
